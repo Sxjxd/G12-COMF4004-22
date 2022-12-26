0097290a909b3fe5400a9826990b55bf178c3cde
Sajid Mansoor <cb009555@students.apiit.lk>
2022-12-26 06:21:33 +0530

    testing

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.get("/getUsers", (req, res) => {
 
 app.post("/createUser", async (req, res) => {
     const user = req.body;
-    const newUser = new.UserModel(user);
+    const newUser = UserModel(user);
     await newUser.save();
 
     res.json(user);
